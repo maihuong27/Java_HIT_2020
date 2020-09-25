a55070ea61726feb82d8f02162367e07747c89a5
Mai Hường <maihuongyp27@gmail.com>
2020-09-25 22:55:25 +0700

    First commit

---
+++ Day_1/src/Bai1.java
@@ -0,0 +1,14 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        int m = 5, n = 6;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m-1 || j == 0 || j == n-1)
+                    System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day_1/src/Bai2.java
@@ -0,0 +1,24 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println("*       *     *     * * * *   * * * *   *     *");
+        System.out.println("|       |    / \\    |      *  |      *   \\   /");
+        System.out.println("* ----- *   *---*   * * * *   * * * *      *");
+        System.out.println("|       |  /     \\  |         |            |");
+        System.out.println("*       * *       * *         *            *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*    *  ******  *       *       *");
+        System.out.println("| \\  |  |        \\     / \\     /");
+        System.out.println("*  * *  ******    *   *   *   *");
+        System.out.println("|   \\|  |          \\ /     \\ /");
+        System.out.println("*    *  ******      *       *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*   *  ******      *      * * * * ");
+        System.out.println(" \\ /   |          / \\     |       *");
+        System.out.println("  *    ******    *---*    * * * * ");
+        System.out.println("  |    |        /     \\   |    \\");
+        System.out.println("  *    ******  *       *  *     *");
+
+    }
+}
