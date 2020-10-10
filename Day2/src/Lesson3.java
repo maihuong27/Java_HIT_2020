6b929e35b0109e642e4498e1bcfa071f24182e11
Mai Hường <maihuongyp27@gmail.com>
2020-10-10 22:30:24 +0700

    update

--- Day2/src/Lesson3.java
+++ Day2/src/Lesson3.java
@@ -17,79 +17,83 @@ public class Lesson3 {
 
         int x = sc.nextInt();
         int[] arr = new int[n + 1];
-
-        if (x == 1) {
-            System.out.println("Nhập mảng: ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-            }
-            System.out.println("Lựa chọn công việc cần làm: ");
-            x = sc.nextInt();
-        }
-
-        else if(x == 2) {
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            System.out.println("Lựa chọn công việc cần làm: ");
-            x = sc.nextInt();
-        }
-
-        if (x == 3) {
-            System.out.print("Nhập vào vị trí k: ");
-            int k = sc.nextInt();
-            System.out.print("Nhập vào giá trị chèn: ");
-            int u = sc.nextInt();
-
-
-            for (int i = n; i >= k; i--){
-                arr[i] = arr[i - 1];
-            }
-            arr[k-1] = u;
-
-            for (int i = 0; i <= n; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            System.out.println("Lựa chọn công việc cần làm: ");
-            x = sc.nextInt();
-        }
-
-        else if(x == 4) {
-            System.out.print("Nhập vào vị trí k: ");
-            int k = sc.nextInt();
-
-            for (int i = k - 1; i < n; i++) {
-                arr[i] = arr[i + 1];
+        while (x <= 7 || x >= 1) {
+            if (x == 1) {
+                System.out.println("Nhập mảng: ");
+                for (int i = 0; i < n; i++) {
+                    arr[i] = sc.nextInt();
+                }
+                System.out.println("Lựa chọn công việc cần làm: ");
+                x = sc.nextInt();
             }
 
-            for (int i = 0; i < n - 1; i++) {
-                System.out.print(arr[i] + " ");
+            else if (x == 2) {
+                for (int i = 0; i < arr.length - 1; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+                System.out.println();
+                System.out.println("Lựa chọn công việc cần làm: ");
+                x = sc.nextInt();
             }
-            System.out.println();
-            System.out.println("Lựa chọn công việc cần làm: ");
-            x = sc.nextInt();
-        }
-        else if(x == 5) {
-            for (int i = n - 1; i >= 0; i--) {
-                System.out.print(arr[i] + " ");
+
+            else if (x == 3) {
+                System.out.print("Nhập vào vị trí k: ");
+                int k = sc.nextInt();
+                System.out.print("Nhập vào giá trị chèn: ");
+                int u = sc.nextInt();
+
+                if (k == n) arr[n + 1] = u;
+                else {
+                    for (int i = n; i >= k; i--){
+                        arr[i] = arr[i - 1];
+                    }
+                    arr[k-1] = u;
+                }
+
+                for (int i = 0; i <= n; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+                System.out.println();
+                System.out.println("Lựa chọn công việc cần làm: ");
+                x = sc.nextInt();
             }
-        }
-        else if(x == 6) {
-            System.out.println(arr[1]);
-            for (int i = 0; i < n; i++) {
-                if (arr[i] % arr[1] == 0 && arr[i] != arr[1]) {
-                    System.out.println(arr[i] + " ");
+
+            else if(x == 4) {
+                System.out.print("Nhập vào vị trí k: ");
+                int k = sc.nextInt();
+
+                for (int i = k - 1; i < n; i++) {
+                    arr[i] = arr[i + 1];
+                }
+
+                for (int i = 0; i < n - 1; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+                System.out.println();
+                System.out.println("Lựa chọn công việc cần làm: ");
+                x = sc.nextInt();
+            }
+            else if(x == 5) {
+                for (int i = n - 1; i >= 0; i--) {
+                    System.out.print(arr[i] + " ");
                 }
             }
-            System.out.println();
-            System.out.println("Lựa chọn công việc cần làm: ");
-            x = sc.nextInt();
+            else if(x == 6) {
+                System.out.println(arr[1]);
+                for (int i = 0; i < n; i++) {
+                    if (arr[i] % arr[1] == 0 && arr[i] != arr[1]) {
+                        System.out.println(arr[i] + " ");
+                    }
+                }
+                System.out.println();
+                System.out.println("Lựa chọn công việc cần làm: ");
+                x = sc.nextInt();
+            }
+            else if(x == 7) {
+                System.exit(0);
+            }
         }
-        else if(x == 7) {
-            System.exit(0);
-        }
+
     }
 }
 
